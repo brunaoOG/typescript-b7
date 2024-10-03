c8f6d10bd0100d5c743eff1981f96f5bd9eb4d68
Bruno da Silva B. <brunooh435yt@gmail.com>
2024-10-03 16:24:51 -0300

    mod2-2/primeiras-tipagens

--- public/script.js
+++ public/script.js
@@ -1,8 +1,3 @@
-var num1 = document.querySelector("#num1");
-var num2 = document.querySelector("#num2");
-var btn = document.querySelector("button");
-var result = document.querySelector(".resultado ");
-var somar = function (x, y) { return x + y; };
-btn.addEventListener("click", function () {
-    result.innerHTML = somar(+num1.value, +num2.value).toString();
-});
+var nome = "Miller";
+var idade = 16;
+var viado = true;
--- src/script.ts
+++ src/script.ts
@@ -1,10 +1,3 @@
-let num1 = document.querySelector("#num1") as HTMLInputElement;
-let num2 = document.querySelector("#num2") as HTMLInputElement;
-let btn = document.querySelector("button") as HTMLButtonElement;
-let result = document.querySelector(".resultado ") as HTMLDivElement;
-
-const somar = (x: number, y: number) => x + y;
-
-btn.addEventListener("click", () => {
-  result.innerHTML = somar(+num1.value, +num2.value).toString();
-});
+let nome: string = "Miller";
+let idade: number = 16;
+let viado: boolean = true;
