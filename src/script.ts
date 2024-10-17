6fd6871f22eb2277a1ba68f664fbbe9a197eebe0
Bruno da Silva B. <brunooh435yt@gmail.com>
2024-10-16 22:20:59 -0300

    mod2-12/type-assertions

--- src/script.ts
+++ src/script.ts
@@ -1,22 +1,3 @@
-//type User = { name: string; level: number };
-interface User {
-  name: string;
-}
+let input = document.querySelector("input") as HTMLInputElement;
 
-let fillSausage = true;
-
-interface User {
-  level: number;
-}
-
-function resume(user: User) {
-  return `Olá! Seja bem-vindo ao server, ${user.name}. 
-  Seu nível atual é ${user.level}, lute para upar.`;
-}
-
-let user = {
-  name: "Bruno",
-  level: 0,
-};
-
-console.log(resume(user));
+let inputValue = input.value;
