b82b9e85d94d902b46fa47207f75f171df4284f1
Bruno da Silva B. <brunooh435yt@gmail.com>
2024-10-16 22:15:19 -0300

    mod2-11/type-interface

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,15 @@
-function resume(user: { name: string; level: number }) {
+//type User = { name: string; level: number };
+interface User {
+  name: string;
+}
+
+let fillSausage = true;
+
+interface User {
+  level: number;
+}
+
+function resume(user: User) {
   return `Olá! Seja bem-vindo ao server, ${user.name}. 
   Seu nível atual é ${user.level}, lute para upar.`;
 }
